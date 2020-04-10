0940b48cedccdd009d5bd22508f1de573c32b2e6
oops <oops@localhost.localdomain>
2020-04-10 15:23:19 -0400

    ex2 tests ok

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,11 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (
+    HashTable,
+    hash_table_insert,
+    hash_table_remove,
+    hash_table_retrieve,
+    hash_table_resize,
+)
 
 
 class Ticket:
@@ -12,12 +14,32 @@ class Ticket:
         self.destination = destination
 
 
-def reconstruct_trip(tickets, length):
-    hashtable = HashTable(length)
-    route = [None] * length
+# overall plan:
+# use a hash-table to O(1)_time look up connecting flights
+# to reconstruct the travel
+def reconstruct_trip(tickets, number_of_flights):
 
-    """
-    YOUR CODE HERE
-    """
+    # Step 1: make and populate a dictionary/hashtable
+    # make custom hash-table
+    trip_dict = HashTable(number_of_flights)
 
-    pass
+    # put tickets in the hash table(dictionary)
+    for trip in tickets:
+        # add items to custom hash-table
+        hash_table_insert(trip_dict, trip.source, trip.destination)
+
+    # Step 2: re-create your travel list
+    # make travel list
+    travel_sequence_list = []
+
+    # start with None source, first leg of trip
+    last_flight = hash_table_retrieve(trip_dict, "NONE")
+
+    for i in range(number_of_flights - 1):
+        # add flight to list
+        travel_sequence_list.append(last_flight)
+
+        # iterate
+        last_flight = hash_table_retrieve(trip_dict, last_flight)
+
+    return travel_sequence_list
